69867735e17962ba7aee85278ead3a15bc7e412c
cghislai <charlyghislain@gmail.com>
2018-12-12 20:06:10 +0200

    Fix multiExceptions

--- dispatcher-api/src/main/java/com/charlyghislain/dispatcher/api/exception/MultipleDispatchingErrorsException.java
+++ dispatcher-api/src/main/java/com/charlyghislain/dispatcher/api/exception/MultipleDispatchingErrorsException.java
@@ -1,13 +1,10 @@
 package com.charlyghislain.dispatcher.api.exception;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MultipleDispatchingErrorsException extends DispatcherException {
 
-    private List<Throwable> renderingExceptions = new ArrayList<>();
 
     public MultipleDispatchingErrorsException(String message) {
         super(message);
@@ -15,35 +12,13 @@ public class MultipleDispatchingErrorsException extends DispatcherException {
 
     public MultipleDispatchingErrorsException(String message, Throwable... causes) {
         super(message);
-        Arrays.stream(causes).forEach(renderingExceptions::add);
+        Arrays.stream(causes).forEach(this::addSuppressed);
     }
 
     public MultipleDispatchingErrorsException(String message, List<? extends Throwable> errors) {
         super(message);
-        this.renderingExceptions.addAll(errors);
-    }
-
-    public List<Throwable> getRenderingExceptions() {
-        return renderingExceptions;
-    }
-
-    @Override
-    public String getMessage() {
-        return super.getMessage();
+        errors.forEach(this::addSuppressed);
     }
 
 
-    @Override
-    public void printStackTrace(PrintWriter s) {
-        super.printStackTrace(s);
-
-        int internalErrors = renderingExceptions.size();
-        s.println("MultiException:: " + internalErrors + "  internal dispatching exceptions: ");
-        for (int index = 0; index < internalErrors; index++) {
-            Throwable cause = renderingExceptions.get(index);
-            s.println("  - " + index + ": " + cause.getMessage());
-            cause.printStackTrace(s);
-        }
-        s.println("MultiException:: completed causes enumeration");
-    }
 }
--- dispatcher-api/src/main/java/com/charlyghislain/dispatcher/api/exception/MultipleRenderingErrorsException.java
+++ dispatcher-api/src/main/java/com/charlyghislain/dispatcher/api/exception/MultipleRenderingErrorsException.java
@@ -1,13 +1,10 @@
 package com.charlyghislain.dispatcher.api.exception;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MultipleRenderingErrorsException extends MessageRenderingException {
 
-    private List<MessageRenderingException> renderingExceptions = new ArrayList<>();
 
     public MultipleRenderingErrorsException(String message) {
         super(message);
@@ -15,35 +12,12 @@ public class MultipleRenderingErrorsException extends MessageRenderingException 
 
     public MultipleRenderingErrorsException(String message, MessageRenderingException... causes) {
         super(message);
-        Arrays.stream(causes).forEach(renderingExceptions::add);
+        Arrays.stream(causes).forEach(this::addSuppressed);
     }
 
     public MultipleRenderingErrorsException(String message, List<MessageRenderingException> renderingExceptions) {
         super(message);
-        this.renderingExceptions = renderingExceptions;
+        renderingExceptions.forEach(this::addSuppressed);
     }
 
-    public List<MessageRenderingException> getRenderingExceptions() {
-        return renderingExceptions;
-    }
-
-    @Override
-    public String getMessage() {
-        return super.getMessage();
-    }
-
-
-    @Override
-    public void printStackTrace(PrintWriter s) {
-        super.printStackTrace(s);
-
-        int internalErrors = renderingExceptions.size();
-        s.println("MultiException:: " + internalErrors + "  internal rendering exceptions: ");
-        for (int index = 0; index < internalErrors; index++) {
-            MessageRenderingException cause = renderingExceptions.get(index);
-            s.println("  - " + index + ": " + cause.getMessage());
-            cause.printStackTrace(s);
-        }
-        s.println("MultiException:: completed causes enumeration");
-    }
 }
